555a9011a55dc7a5b115e2b3ec3dfef3aa660a0a
riaz <riaz.2012@gmail.com>
2024-04-21 11:18:34 -0700

    added tests for quick union

--- Python/Algorithms/algo/unionfind/quickunion.py
+++ Python/Algorithms/algo/unionfind/quickunion.py
@@ -5,10 +5,29 @@ class QuickUnion(UF):
         super().__init__(n)
     
     def connected(self, p: int, q: int) -> bool:
-        return False
+        """
+        If the two nodes have the same root, they are connected
+        since that is the condition for the union
+        """
+        return self.__root(p) == self.__root(q)
     
     def union(self, p: int, q: int) -> None:
-        return None
+        """
+        This is the quick union function
+        We set the root of p to the root of q
+        """
+        root_p = self.__root(p)
+        root_q = self.__root(q)
+        self.arr[root_p] = root_q
     
     def num_of_components(self) -> int:
-        return len(set(self.arr))
+        return len({self.__root(node) for node in self.arr})        
+    
+    def __root(self, id: int) -> int:
+        """
+        This method finds the root of a node 
+        """
+        while id != self.arr[id]:
+            id = self.arr[id]
+        return id
+    
--- Python/Algorithms/tests/unionfind/test_quickunion.py
+++ Python/Algorithms/tests/unionfind/test_quickunion.py
@@ -1,8 +1,53 @@
 from algo.unionfind.quickunion import QuickUnion
 
 def test_quickunion():
-    uf = QuickUnion(10)
+    N = 10
+    uf = QuickUnion(N)
 
     assert len(uf.arr) == 10 # note: the array was set by the ABC
 
     assert uf.arr == list(range(len(uf.arr))) # this is what the constructor would assign
+
+    uf.union(0,1)
+    uf.union(5,9)
+    uf.union(3,4)
+
+    assert uf.connected(0,1) == True
+    
+    assert uf.connected(0,5) == False
+
+    assert uf.connected(7,8) == False
+
+    assert uf.num_of_components() == 7
+
+    uf.union(0,2)
+    uf.union(7,8)
+
+    assert uf.connected(0,2) == True
+
+    assert uf.num_of_components() == 5
+
+    uf.union(0, 4)
+
+    assert uf.connected(2, 3) == True
+
+    assert uf.num_of_components() == 4
+
+    uf.union(3, 6)
+    uf.union(8, 6)
+
+    assert uf.num_of_components() == 2
+
+    assert uf.connected(1,7) == True
+
+    uf.union(7, 9)
+
+    assert uf.num_of_components() == 1
+
+    # At this point everything is connected
+
+    assert uf.connected(1,1) # reflection
+
+    assert uf.connected(3, 4) == uf.connected(4, 3) # symmetry
+
+    assert uf.connected(1,2) == uf.connected(2, 5) == uf.connected(5, 7) == uf.connected(1,7) # equivalence
