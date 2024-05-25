62fdeaaa0324835f7e36aed20e34454ff725db3c
riaz <riaz.2012@gmail.com>
2024-05-25 06:08:19 -0700

    new problems

--- Python/Leetcode/problems/largest_number_twice_of_others.py
+++ Python/Leetcode/problems/largest_number_twice_of_others.py
@@ -1,4 +1,13 @@
-def largest_atleast_twice():
+def largest_atleast_twice(arr: list[int]) -> int:
     """
-    
+    pick the largest in the array
+    if the largest is atleast 2x greater or equal to rest return index of largest 
+    else return -1
     """
+
+    n = len(arr)
+    index_max = max(range(n), key=arr.__getitem__)
+    for i in range(n):
+        if i != index_max and (2 * arr[i] ) > arr[index_max]:
+                return -1
+    return index_max
---
+++ Python/Leetcode/tests/test_largest_number_twice_of_other.py
@@ -0,0 +1,5 @@
+from problems import largest_atleast_twice
+
+def test_largest_atleast_twice():
+    assert largest_atleast_twice([3,6,1,0]) == 1
+    assert largest_atleast_twice([1,2,3,4]) == -1
